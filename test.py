8ad51d5bbd49b3cbd3aedba1167a85a4d8d7426e
ParieKumar <pkumar@overlake.org>
2022-07-11 10:22:41 -0700

    string to date time

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import urllib.request
 import matplotlib.pyplot as pp
 import numpy as np
- 
+#from datetime import datetime
+from dateutil import parser 
 
 # The URL to get the Quantum Lab Data
-url = "https://datadropper.z13.web.core.windows.net/data.csv"
+url = "https://datadropper.z13.web.core.windows.net/flow.csv"
  
 # read the data from the URL into file and write it to a local file labdata.txt
  
@@ -32,9 +33,10 @@ Y=[]
 for i in range(len(data)):
     for j in range(2):
         if j == 0:
-            X.append(data[i][j])
+            #X.append(datetime.strptime(data[i][j], '%d/%m/%y %H:%M:%S'))
+            X.append(parser.parse(data[i][j]))
         else:
-            Y.append(data[i][j])
+            Y.append(float(data[i][j]))
  
 print(X)
 print(Y)
